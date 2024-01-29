b61f1a36a1656febfc68eb4f4ccd96eae263af90
ngtddat <nguyenopp41@gmail.com>
2024-01-29 22:20:36 +0700

    repair w1

--- tuan1/app/src/main/java/com/example/myapplicationtuan1/tuan11/Tuan11MainActivity.java
+++ tuan1/app/src/main/java/com/example/myapplicationtuan1/tuan11/Tuan11MainActivity.java
@@ -1,39 +1,47 @@
-package com.example.myapplication.tuan1;
+package com.example.myapplicationtuan1.tuan11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.EditText;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.myapplicationtuan1.R;
 
-import com.example.myapplication.R;
-
-public class Tuan1MainActivity extends AppCompatActivity {
-    EditText txt1, txt2;
+public class Tuan11MainActivity extends AppCompatActivity {
+    //Khai bao cac control
+    EditText txt1,txt2;
     Button btn1;
     TextView tv1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tuan1_main);
+        setContentView(R.layout.activity_tuan11_main);
+
         txt1=findViewById(R.id.demo11txt1);
         txt2=findViewById(R.id.demo11txt2);
         btn1=findViewById(R.id.demo11Btn1);
         tv1=findViewById(R.id.demo11Tv1);
-
+        //Xu ly su kien
         btn1.setOnClickListener(v->{
+            //Goi ham tinh tong
             tinhTong();
         });
     }
 
+    //Dinh nghia ham tinh tong
     private void tinhTong() {
+
         String str1=txt1.getText().toString();
         float so1=Float.parseFloat(str1);
+
         String str2=txt2.getText().toString();
         float so2=Float.parseFloat(str2);
-        float tong=so1+so2;
+        //tinh tong
+        float tong= so1 + so2;
+
         tv1.setText(String.valueOf(tong));
     }
 }
